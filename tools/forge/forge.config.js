45267680f5c5340bcff15e5d630e1f8c92250d8b
guasam <guasam@codesbiome.com>
2022-03-25 15:16:10 +0530

    Update exe name

--- tools/forge/forge.config.js
+++ tools/forge/forge.config.js
@@ -8,7 +8,7 @@ module.exports = {
     // Create asar archive for main, renderer process files
     asar: true,
     // Set executable name
-    executableName: 'ERWT Boilerplate',
+    executableName: 'EmGui Boilerplate',
     // Set application copyright
     appCopyright: 'Copyright (C) 2021 Codesbiome, Guasam',
   },
